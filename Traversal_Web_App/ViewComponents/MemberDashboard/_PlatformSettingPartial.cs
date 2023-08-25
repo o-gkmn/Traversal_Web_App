b435d5717c3a7c236170d000d7d9f7f769fc0aff
o-gkmn <ozgur.gokmen735@gmail.com>
2023-08-25 20:34:22 +0300

    Dahsboard is edited

---
+++ Traversal_Web_App/ViewComponents/MemberDashboard/_PlatformSettingPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Traversal_Web_App.ViewComponents.MemberDashboard
+{
+    public class _PlatformSettingPartial : ViewComponent
+    {
+
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
